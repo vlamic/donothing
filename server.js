dcece8b0ec2aeea258ad433558c89e2c79dfee75
Vladislav Mickevic <vladislav.mickevic@sticky.ad>
2013-07-23 09:20:18 +0200

    package.json is added

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 var fs = require('fs');
+var express = require('express');
 
 fs.appendFile('api.log', 'Application started at ' + new Date().toISOString() + '\n', function (err) {
 
 });
+
+app.get('/', function(req, res){
+  res.send(200);
+});
+
+
+app.listen(80);
